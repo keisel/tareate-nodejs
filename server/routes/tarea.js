c53bb03a4be0d3a085050dbd2527929bce86eb42
Keisel Barreto <keiselbarreto@gmail.com>
2019-01-13 04:20:11 -0400

    Problemas de carga de archivos resuelto

--- server/routes/tarea.js
+++ server/routes/tarea.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const Tarea = require('../models/tarea');
 const _ = require('underscore');
-const fileUpload = require('express-fileupload');
-app.use(fileUpload());
+
 
 // esto solo lo pdora hacer el admin
 app.get('/tarea', (req, res) => {
@@ -61,65 +60,7 @@ app.get('/tarea/:id', (req, res) => {
         });
 });
 
-// esto lo podra hacer el usuario
-app.post('/tarea', (req, res) => {
 
-    let body = req.body;
-
-    if (req.files) {
-        var archivo = req.files.tarea;
-        let nombreCortado = archivo.name.split('.');
-        let extension = nombreCortado[nombreCortado.length - 1];
-        let extensionesValidas = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xlsx', 'xlsm'];
-
-        if (extensionesValidas.indexOf(extension) < 0) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'Error. Las extensiones permitidas son: ' + extensionesValidas.join(', ')
-                }
-            });
-        }
-        body.tarea = `${nombreCortado[0]}-${new Date().getMilliseconds() }.${extension}`;
-    }
-    let tarea = new Tarea({
-        titulo: body.titulo,
-        usuario: body.usuario,
-        descripcion: body.descripcion,
-        categoria: body.categoria,
-        fechaEntrega: body.fechaEntrega,
-        tarea: body.tarea
-    })
-    tarea.save((err, tareaDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            });
-        } else {
-            if (req.files) {
-                archivo.mv(`uploads/tareas/${body.tarea}`, (err) => {
-                    if (err) {
-                        return res.status(500).json({
-                            ok: false,
-                            err
-                        });
-                    } else {
-                        return res.status(200).json({
-                            ok: true,
-                            tarea: tareaDB
-                        })
-                    }
-                });
-            } else {
-                return res.status(200).json({
-                    ok: true,
-                    tarea: tareaDB
-                })
-            }
-        }
-    });
-});
 // este id lo agarro por el token
 app.get('/tarea/usuario', (req, res) => {
     let id = req.usuario._id;
@@ -174,68 +115,7 @@ app.get('/tarea/usuario/:id', (req, res) => {
         });
 });
 
-// este es el put
-app.post('/tareas/:id', (req, res) => {
 
-    let id = req.params.id;
-    let body = req.body;
-    let id_usuario = body.usuario;
-    body = _.pick(body, ['titulo', 'descripcion', 'categoria', 'fechaEntrega', 'tarea']);
-    console.log(req.files);
-    if (req.files) {
-        var archivo = req.files.tarea;
-        let nombreCortado = archivo.name.split('.');
-        let extension = nombreCortado[nombreCortado.length - 1];
-        let extensionesValidas = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xlsx', 'xlsm'];
-
-        if (extensionesValidas.indexOf(extension) < 0) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'Error. Las extensiones permitidas son: ' + extensionesValidas.join(', ')
-                }
-            });
-        }
-        body.tarea = `${nombreCortado[0]}-${new Date().getMilliseconds() }.${extension}`;
-    }
-
-    Tarea.findOneAndUpdate({ _id: id, usuario: id_usuario }, body, { new: true, runValidators: true, context: 'query' }, (err, tareaDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            });
-        } else if (!tareaDB) {
-            return res.status(400).json({
-                ok: false,
-                err: {
-                    message: 'id No encontrado'
-                }
-            });
-        } else {
-            if (req.files) {
-                archivo.mv(`uploads/tareas/${body.tarea}`, (err) => {
-                    if (err) {
-                        return res.status(500).json({
-                            ok: false,
-                            err
-                        });
-                    } else {
-                        return res.status(200).json({
-                            ok: true,
-                            tarea: tareaDB
-                        })
-                    }
-                });
-            } else {
-                return res.status(200).json({
-                    ok: true,
-                    tarea: tareaDB
-                })
-            }
-        }
-    });
-});
 
 
 app.delete('/tarea/:id', (req, res) => {
--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const app = express();
 const Descarga = require('../models/descarga');
-const fileUpload = require('express-fileupload');
+const Tarea = require('../models/tarea');
 const _ = require('underscore');
 const fs = require('fs');
 const path = require('path');
-
+const fileUpload = require('express-fileupload');
 app.use(fileUpload());
+
 
 // descarga
 app.post('/descarga', (req, res) => {
 
-    if (!req.files) {
+    if (Object.keys(req.files).length == 0) {
         return res.status(400)
             .json({
                 ok: false,
                 err: {
-                    message: 'Debe cargar un archivo carajo'
+                    message: 'Debe cargar un archivo'
                 }
             });
     }
 
     let archivo = req.files.tarea;
-    let nombreArchivo = validarExtension(archivo, res);
+    let nombreCortado = archivo.name.split('.');
+    let extension = nombreCortado[nombreCortado.length - 1];
+    let extensionesValidas = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xlsx', 'xlsm'];
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Error. Las extensiones permitidas son: ' + extensionesValidas.join(', ')
+            }
+        });
+    }
+    let nombreArchivo = `${nombreCortado[0]}-${new Date().getMilliseconds() }.${extension}`;
 
     let body = req.body;
     let descarga = new Descarga({
@@ -36,43 +49,135 @@ app.post('/descarga', (req, res) => {
                 ok: false,
                 err
             });
-        } else {
-            cargarArchivo(archivo, nombreArchivo, res, tareaDB);
         }
+        cargarArchivo(archivo, nombreArchivo, res, tareaDB, 'descargas');
     })
 });
 
-function validarExtension(archivo, res) {
-    let nombreCortado = archivo.name.split('.');
-    let extension = nombreCortado[nombreCortado.length - 1];
-    let extensionesValidas = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xlsx', 'xlsm'];
 
-    if (extensionesValidas.indexOf(extension) < 0) {
-        return res.status(400).json({
-            ok: false,
-            err: {
-                message: 'Error. Las extensiones permitidas son: ' + extensionesValidas.join(', ')
-            }
-        });
+// POST TAREA esto lo podra hacer el usuario
+app.post('/tarea', (req, res) => {
+
+    let body = req.body;
+    if (Object.keys(req.files).length > 0) {
+        var archivo = req.files.tarea;
+        let nombreCortado = archivo.name.split('.');
+        let extension = nombreCortado[nombreCortado.length - 1];
+        let extensionesValidas = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xlsx', 'xlsm'];
+
+        if (extensionesValidas.indexOf(extension) < 0) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Error. Las extensiones permitidas son: ' + extensionesValidas.join(', ')
+                }
+            });
+        }
+        body.tarea = `${nombreCortado[0]}-${new Date().getMilliseconds() }.${extension}`;
     }
-    return `${nombreCortado[0]}-${new Date().getMilliseconds() }.${extension}`;
-}
 
-
-function cargarArchivo(archivo, nombreArchivo, res, tareaDB) {
-
-    archivo.mv(`uploads/descargas/${nombreArchivo}`, (err) => {
+    let tarea = new Tarea({
+        titulo: body.titulo,
+        usuario: body.usuario,
+        descripcion: body.descripcion,
+        categoria: body.categoria,
+        fechaEntrega: body.fechaEntrega,
+        tarea: body.tarea
+    })
+    tarea.save((err, tareaDB) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
                 err
             });
+        }
+        if (Object.keys(req.files).length > 0) {
+            cargarArchivo(archivo, body.tarea, res, tareaDB, 'tareas');
         } else {
             return res.status(200).json({
                 ok: true,
                 tarea: tareaDB
             })
         }
+    });
+});
+
+
+// este es el put
+app.post('/tareas/:id', (req, res) => {
+
+    let body = req.body;
+    let id = req.params.id;
+    let id_usuario = body.usuario;
+    body = _.pick(body, ['titulo', 'descripcion', 'categoria', 'fechaEntrega', 'tarea']);
+
+    if (Object.keys(req.files).length > 0) {
+        var archivo = req.files.tarea;
+        let nombreCortado = archivo.name.split('.');
+        let extension = nombreCortado[nombreCortado.length - 1];
+        let extensionesValidas = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'xlsx', 'xlsm'];
+
+        if (extensionesValidas.indexOf(extension) < 0) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Error. Las extensiones permitidas son: ' + extensionesValidas.join(', ')
+                }
+            });
+        }
+        body.tarea = `${nombreCortado[0]}-${new Date().getMilliseconds() }.${extension}`;
+    }
+
+    Tarea.findOneAndUpdate({ _id: id, usuario: id_usuario }, body, { runValidators: true, context: 'query' }, (err, tareaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!tareaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'id No encontrado'
+                }
+            });
+        }
+        if (Object.keys(req.files).length > 0) {
+            borrarArchivo(tareaDB.tarea);
+            cargarArchivo(archivo, body.tarea, res, tareaDB, 'tareas');
+        } else {
+            return res.status(200).json({
+                ok: true,
+                tarea: tareaDB
+            })
+        }
+
+    });
+});
+
+
+function borrarArchivo(tarea) {
+
+    let pathArchivo = path.resolve(__dirname, `../../uploads/tareas/${tarea}`);
+    if (fs.existsSync(pathArchivo)) {
+        fs.unlinkSync(pathArchivo);
+    }
+    return;
+}
+
+function cargarArchivo(archivo, nombreArchivo, res, tareaDB, tipo) {
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, (err) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        return res.status(200).json({
+            ok: true,
+            tarea: tareaDB
+        })
     });
 }
 
