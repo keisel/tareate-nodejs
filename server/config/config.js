cee470a67b70e9efa2b897ab98ab57dfc046703b
Keisel Barreto <keiselbarreto@gmail.com>
2019-01-17 00:59:37 -0400

    Login google listo, ya solo falta el chat y las optimizaciones

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,5 @@ process.env.CADUCIDAD_TOKEN = '48h';
 // semilla de autenticacion
 
 process.env.SEED = process.env.SEED || 'tareateKeisel';
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '1090425564135-atsso83du4jdf0lcum1epkftnii1ios9.apps.googleusercontent.com';
--- server/routes/login.js
+++ server/routes/login.js
@@ -3,7 +3,8 @@ const app = express();
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
 app.post('/login', (req, res) => {
 
@@ -44,6 +45,91 @@ app.post('/login', (req, res) => {
 
                 });
             });
+    });
+});
+
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({ idToken: token, audience: process.env.CLIENT_ID, });
+    const payload = ticket.getPayload();
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        google: true
+    }
+}
+
+app.post('/google', async(req, res) => {
+
+    let token = req.body.idtoken;
+    let googleUser = await verify(token)
+        .catch(e => {
+            res.status(403).json({
+                ok: false,
+                err: {
+                    message: 'Token invalido'
+                }
+            });
+        });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Datos invalidos'
+                }
+            });
+        };
+        if (usuarioDB) {
+
+            if (usuarioDB.google == false) {
+
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Ud no esta autenticado con google debe usar su autenticacion normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            }
+        } else {
+
+            let usuario = new Usuario({
+                nombre: googleUser.nombre,
+                email: googleUser.email,
+                google: true,
+                password: ':)'
+            });
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            });
+        }
     });
 });
 
